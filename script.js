9ebb11f22c28b42828acebc766af3c7ec2071dde
Pranisha4841 <pransamrzn@gmail.com>
2026-02-20 20:29:59 +0545

    update

--- script.js
+++ script.js
@@ -3,35 +3,31 @@ $(document).ready(function () {
     var name = $("#name").val();
     var age = $("#age").val();
     var course = $("#course").val();
-  });
-  document.getElementById("addBtn").addEventListener("click", function () {
-    var name = document.getElementById("name").value;
-    var age = document.getElementById("age").value;
-    var course = document.getElementById("course").value;
-  });
-  if (name == "" || age == "" || course == "") {
-    alert("Please fill all fields");
-    return;
-  }
 
-  $("#studentTable").append(
-    "<tr>" +
-      "<td>" +
-      name +
-      "</td>" +
-      "<td>" +
-      age +
-      "</td>" +
-      "<td>" +
-      course +
-      "</td>" +
-      "<td><button class='deleteBtn'>Delete</button></td>" +
-      "</tr>",
-  );
+    if (name == "" || age == "" || course == "") {
+      alert("Please fill all fields");
+      return;
+    }
 
-  $("#name").val("");
-  $("#age").val("");
-  $("#course").val("");
+    $("#studentTable").append(
+      "<tr>" +
+        "<td>" +
+        name +
+        "</td>" +
+        "<td>" +
+        age +
+        "</td>" +
+        "<td>" +
+        course +
+        "</td>" +
+        "<td><button class='deleteBtn'>Delete</button></td>" +
+        "</tr>",
+    );
+
+    $("#name").val("");
+    $("#age").val("");
+    $("#course").val("");
+  });
 
   $(document).on("click", ".deleteBtn", function () {
     $(this).closest("tr").remove();
