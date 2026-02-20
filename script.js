c9838d137bd519dc070ef9a9874375eee6cc9f08
Pranisha4841 <pransamrzn@gmail.com>
2026-02-20 20:07:47 +0545

    update

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
-  var name = $("#name").val();
-  var age = $("#age").val();
-  var course = $("#course").val();
+  $("#addBtn").click(function () {
+    var name = $("#name").val();
+    var age = $("#age").val();
+    var course = $("#course").val();
+  });
+});
+if (name == "" || age == "" || course == "") {
+  alert("Please fill all fields");
+  return;
+}
+$("#name").val("");
+$("#age").val("");
+$("#course").val("");
+$(document).on("click", ".deleteBtn", function () {
+  $(this).closest("tr").remove();
+});
+$(document).on("click", ".editBtn", function () {
+  var row = $(this).closest("tr");
+  var name = row.find("td:eq(0)").text();
+  var age = row.find("td:eq(1)").text();
+  var course = row.find("td:eq(2)").text();
+  $("#name").val(name);
+  $("#age").val(age);
+  $("#course").val(course);
+  row.remove();
 });
